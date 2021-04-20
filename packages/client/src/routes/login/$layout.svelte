d9c7ccd7a0393120a887617d45fec2940c45c74f
Mervin <mervinjin@gmail.com>
2021-04-20 23:30:35 +0800

    chore: init

---
+++ packages/client/src/routes/login/$layout.svelte
@@ -0,0 +1,10 @@
+<div class="login-container">
+  <slot />
+</div>
+
+<style lang="postcss" scoped>
+  .login-container {
+    @apply w-full h-full;
+    @apply grid justify-center content-center;
+  }
+</style>
---
+++ packages/client/src/routes/login/index.svelte
@@ -0,0 +1,18 @@
+<div class="accely-logo">
+  <img class="logoicon" alt="logo" src="/logo.png" />
+  <span class="logotitle">Accely</span>
+</div>
+
+<style lang="postcss">
+  .accely-logo {
+    @apply w-full grid justify-center gap-4;
+  }
+
+  .accely-logo > .logoicon {
+    @apply w-36;
+  }
+
+  .accely-logo > .logotitle {
+    @apply text-4xl font-mono;
+  }
+</style>
