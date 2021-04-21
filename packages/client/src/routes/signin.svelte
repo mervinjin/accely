e8edb12ffa28e499c803321ff6c61ccc124e2999
Mervin <mervinjin@gmail.com>
2021-04-21 14:28:18 +0800

    feat: resize logo

--- packages/client/src/routes/signin.svelte
+++ packages/client/src/routes/signin.svelte
@@ -36,11 +36,11 @@
     @apply w-full grid justify-center justify-items-center gap-4;
 
     > .logo {
-      @apply w-24;
+      @apply w-32;
     }
 
     > .title {
-      @apply text-2xl text-center font-mono;
+      @apply text-3xl text-center font-mono;
     }
   }
 
@@ -48,7 +48,7 @@
     @apply grid justify-center gap-4;
 
     .input {
-      @apply px-4 py-2 w-72 border-b-2 rounded;
+      @apply px-4 py-2 w-72 border-b;
     }
 
     .submitbutton {
