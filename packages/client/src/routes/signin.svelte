14b52d711e18913b8d2f5045dd9932e0a776224b
Mervin <mervinjin@gmail.com>
2021-04-21 11:13:54 +0800

    build(client): support postcss-nested

--- packages/client/src/routes/signin.svelte
+++ packages/client/src/routes/signin.svelte
@@ -1,4 +1,4 @@
-<div class="login-container">
+<div class="signin-cotainer">
   <div class="logo-box">
     <img class="logo" alt="logo" src="/logo.svg" />
     <span class="title">Accely</span>
@@ -6,23 +6,20 @@
 </div>
 
 <style lang="postcss">
-  .login-container {
+  .signin-cotainer {
     @apply w-full h-full;
-    @apply grid justify-center content-center;
+    @apply grid justify-center content-center items-center;
   }
 
   .logo-box {
     @apply w-full grid justify-center gap-4;
 
-    .logo {
+    > .logo {
+      @apply w-36;
     }
-  }
 
-  .logo-box > .logo {
-    @apply w-36;
-  }
-
-  .logo-box > .title {
-    @apply text-4xl font-mono;
+    > .title {
+      @apply text-4xl text-center font-mono;
+    }
   }
 </style>
