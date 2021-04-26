8fb857536ac928ecc8d9d2cab321443a19755784
Vache <mervinjin@gmail.com>
2021-04-26 08:37:42 +0800

    chore(client): add notification component (#4)
    * feat: logic
    
    * chore(client): message component
    
    * chore: add remove and removeAll function

--- packages/client/src/routes/$layout.svelte
+++ packages/client/src/routes/$layout.svelte
@@ -1,5 +1,8 @@
 <script>
+  import { MessageBox } from '$lib/components/message'
   import '../app.postcss'
 </script>
+
+<MessageBox />
 
 <slot />
--- packages/client/src/routes/index.svelte
+++ packages/client/src/routes/index.svelte
@@ -1,4 +1,11 @@
+<script>
+  import { message } from '$lib/components/message'
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+
+<button on:click={() => message.info('this is a message')}
+  >create message</button
+>
+
+<slot />
